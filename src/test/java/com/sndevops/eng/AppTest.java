3c501ef4709f7b0f3365bd621b177a0275c9670d
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-06 13:27:02 -0800

    Removing Commit Test Scenario 1

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,10 +7,6 @@ public class AppTest {
     private String INPUT = "1234568910";
     @Test 
     public void testLength() {
-        // new commit
-        //
         Assert.assertEquals(INPUT,"1234568910");
-        //
-        //
     }
 }
