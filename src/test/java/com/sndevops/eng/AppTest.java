99fcb154a7b4c57f4e006f66b76cac31c19b5802
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-06 15:56:15 -0800

    Test Scenario 1

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class AppTest {
 
-    private String INPUT = "12345689";
+    private String INPUT = "12";
     @Test 
     public void testLength() { 
-        Assert.assertEquals(INPUT,"12345689");
+        Assert.assertEquals(INPUT,"12");
     }
 }
