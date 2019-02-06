038125d7edb49c181ce13e1c550d5e94f79aebec
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-05 17:21:22 -0800

    Removed Lines Perli Dev 4 Scenario 1

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,10 +7,6 @@ public class AppTest {
     private String INPUT = "123456";
     @Test 
     public void testLength() {
-        // test
-        // again
-        // Ok
-        // Again Test
         Assert.assertEquals(INPUT,"123456");
     }
 }
