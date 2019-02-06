d9d0b2a4edfda014c9180174f3ae8df845afdc1d
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-05 18:09:35 -0800

    Test scenario run 4

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,6 +7,7 @@ public class AppTest {
     private String INPUT = "12345689";
     @Test 
     public void testLength() {
+        // new commit
         Assert.assertEquals(INPUT,"12345689");
     }
 }
