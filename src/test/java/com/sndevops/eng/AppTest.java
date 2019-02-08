516eb5dcf0c5400bdc04f6a247ec7be5f6bc89e7
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-07 22:08:25 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 public class AppTest {
 
-    private String INPUT = "1 1";
+    private String INPUT = "1 2 1";
     @Test 
     public void testLength() { 
         Assert.assertEquals(INPUT,"1  1");
