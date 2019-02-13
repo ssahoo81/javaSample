16768c1f39344d8e57cab6d9e4c7b10b766dd546
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-13 13:45:16 -0800

    1

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
     private String INPUT = "1   2 3 4 1";
     @Test 
     public void testLength() { 
-        Assert.assertEquals(INPUT,"1        6  1");
+        Assert.assertEquals(INPUT,"1         6  1");
 
     }
 }
