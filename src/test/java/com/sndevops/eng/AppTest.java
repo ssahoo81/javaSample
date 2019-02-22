53745acf7333e21ee896f09b27504a4ed91e5f2b
rrasane <47263869+rrasane@users.noreply.github.com>
2019-02-21 17:20:11 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
     private String INPUT = "1    2  3 4  1";
     @Test 
     public void testLength() { 
-        Assert.assertEquals(INPUT,"1   f   j    1  cancel     8  dr   6   close1");
+        Assert.assertEquals(INPUT,"1   f   j    1  cancel     8   dr   6   close1");
  
     }
 }
