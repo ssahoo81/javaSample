c15c533fd466ae22a643257b75dc14f35859f1a0
rrasane <47263869+rrasane@users.noreply.github.com>
2019-03-04 15:21:56 -0800

    Update AppTest.java

--- src/test/java/com/sndevops/eng/AppTest.java
+++ src/test/java/com/sndevops/eng/AppTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 
 public class AppTest {
 // rrt   
-    private String INPUT = "321";
+    private String INPUT = "3221";
     @Test 
     public void testLength() { 
-        Assert.assertEquals(INPUT,"321"); 
+        Assert.assertEquals(INPUT,"3221"); 
     }
 }
