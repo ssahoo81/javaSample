66ea7ce7f71a6c2acb3c727bdd8406aee2bd916b
rrasane <47263869+rrasane@users.noreply.github.com>
2019-03-11 16:28:46 -0700

    Update App.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -9,8 +9,6 @@ public class App
     public static void main( String[] args )
     {
         // This is a Test
-        // This is a Test
-        // grCallback
         System.out.println( "Hello devops" );
     }
 }
