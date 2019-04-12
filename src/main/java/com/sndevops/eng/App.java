56b9c5c17071ce2c310b06b8182d47278f682c8d
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-04-12 13:47:14 -0700

    updated app.java

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -7,24 +7,7 @@ package com.sndevops.eng;
 public class App
 {
     public static void main( String[] args )
-    {
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // This is a Test
-        // and I'm out...
-        // found a bug with that last commit
-        // fresh instance
-        // second commit
-        //r
-        // again
-        // tgo 
-        System.out.println( "Hello devops455" );
+    { 
+        System.out.println( "Hello devops4534" );
     }
 }
