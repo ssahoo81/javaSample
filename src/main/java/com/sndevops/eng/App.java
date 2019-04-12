70e3e68f5a9b817c9bf9c847fa2e59adc04f28b1
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-04-12 16:19:52 -0700

    com-2

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     { 
-        System.out.println( "Hello devops4534 ttt" );
+        System.out.println( "Hello devops4534 test" );
     }
 }
