1612f8622a3752cb1ec38e1942840c72975a4a27
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-04-15 14:12:28 -0700

    com-6

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     { 
-        System.out.println( "Hello devops4534 901" );
+        System.out.println( "Hello devops4534 902" );
     }
 }
