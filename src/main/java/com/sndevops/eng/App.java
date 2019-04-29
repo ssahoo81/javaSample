c68750b8e9d13170023386640ed517991c509acb
ssahoo81 <49508585+ssahoo81@users.noreply.github.com>
2019-04-29 16:55:38 -0700

    LIP-54 : updated

--- src/main/java/com/sndevops/eng/App.java
+++ src/main/java/com/sndevops/eng/App.java
@@ -4,10 +4,10 @@ package com.sndevops.eng;
  * Hello world!
  *
  */
-public class App
+public class App12
 {
     public static void main( String[] args )
     { 
-        System.out.println( "Hello devops4534 12" );
+        System.out.println( "Hello devops4534 10" );
     }
 }
